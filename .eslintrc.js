51162da298b348a9c774cd69d3bac6824a8d03b0
k-tsunoda615 <kntrtnd@gmail.com>
2025-04-03 20:07:35 +0900

    ESLint & Prettier & Husky & lint-staged のセットアップ
    yarn add -D eslint prettier husky lint-staged
    yarn add -D eslint-plugin-react eslint-plugin-react-hooks
    yarn add -D eslint-config-prettier eslint-plugin-prettier
    yarn add -D @typescript-eslint/eslint-plugin @typescript-eslint/parser

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
